8ec94d67789ff597cec845c20d2d689507d03f05
John-Philip Johansson <seriema@gmail.com>
2016-02-20 16:35:10 +0100

    Simplify dev setup with npm
    npm was already a requirement, so this just makes things easier for
    anyone who wants to help.

--- src/compile.sh
+++
@@ -1,5 +0,0 @@
-# Compile LESS to CSS
-lessc src/scratchblocks2.less > build/scratchblocks2.css
-
-# Compile and minify JS
-uglifyjs src/scratchblocks2.js src/diacritics.js --comments --output build/scratchblocks2.js
