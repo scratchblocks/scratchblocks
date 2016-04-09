9399323e3eac3ead7ebcb9a5c3cac3c2b807809e
tjvr <tim@tjvr.org>
2016-04-09 22:20:47 +0100

    Start writing parser

--- src/extra_strings.py
+++ src/extra_strings.py
@@ -15,112 +15,112 @@ translate.scratch.mit.edu:
 extra_strings = {
 
     "de": { # German
-        "drehe dich nach links um _ Grad": "turn @arrow-ccw _ degrees",
-        "drehe dich nach rechts um _ Grad": "turn @arrow-cw _ degrees",
+        "drehe dich nach links um %n Grad": "turn @arrow-ccw %n degrees",
+        "drehe dich nach rechts um %n Grad": "turn @arrow-cw %n degrees",
         "Wenn die grüne Flagge angeklickt": "when @green-flag clicked",
         "Ende": "end",
     },
 
     "pt": { # Portugese
-        "gira para a esquerda _ º": "turn @arrow-ccw _ degrees",
-        "gira para a direita _ º": "turn @arrow-cw _ degrees",
+        "gira para a esquerda %n º": "turn @arrow-ccw %n degrees",
+        "gira para a direita %n º": "turn @arrow-cw %n degrees",
         "Quando alguém clicar na bandeira verde": "when @green-flag clicked",
         "fim": "end",
     },
 
     "it": { # Italian
-        "ruota in senso antiorario di _ gradi": "turn @arrow-ccw _ degrees",
-        "ruota in senso orario di _ gradi": "turn @arrow-cw _ degrees",
+        "ruota in senso antiorario di %n gradi": "turn @arrow-ccw %n degrees",
+        "ruota in senso orario di %n gradi": "turn @arrow-cw %n degrees",
         "quando si clicca sulla bandiera verde": "when @green-flag clicked",
         "fine": "end",
     },
 
     "fr": { # French
-        "tourner gauche de _ degrés": "turn @arrow-ccw _ degrees",
-        "tourner droite de _ degrés": "turn @arrow-cw _ degrees",
+        "tourner gauche de %n degrés": "turn @arrow-ccw %n degrees",
+        "tourner droite de %n degrés": "turn @arrow-cw %n degrees",
         "quand le drapeau vert pressé": "when @green-flag clicked",
         "fin": "end",
     },
 
     "es": { # Spanish
-        "girar a la izquierda _ grados": "turn @arrow-ccw _ degrees",
-        "girar a la derecha _ grados": "turn @arrow-cw _ degrees",
+        "girar a la izquierda %n grados": "turn @arrow-ccw %n degrees",
+        "girar a la derecha %n grados": "turn @arrow-cw %n degrees",
         "al presionar bandera verde": "when @green-flag clicked",
         "fin": "end",
     },
 
     "nl": { # Dutch
-        "draai _ graden naar links": "turn @arrow-ccw _ degrees",
-        "draai _ graden naar rechts": "turn @arrow-cw _ degrees",
+        "draai %n graden naar links": "turn @arrow-ccw %n degrees",
+        "draai %n graden naar rechts": "turn @arrow-cw %n degrees",
         "wanneer groene vlag wordt aangeklikt": "when @green-flag clicked",
         "einde": "end",
     },
 
     "zh_CN": { # Chinese (simplified)
-        "左转 _ 度": "turn @arrow-ccw _ degrees",
-        "右转 _ 度": "turn @arrow-cw _ degrees",
-        "转动CCW _ 度": "turn @arrow-ccw _ degrees",
-        "转动CW _ 度": "turn @arrow-cw _ degrees",
+        "左转 %n 度": "turn @arrow-ccw %n degrees",
+        "右转 %n 度": "turn @arrow-cw %n degrees",
+        "转动CCW %n 度": "turn @arrow-ccw %n degrees",
+        "转动CW %n 度": "turn @arrow-cw %n degrees",
         "点击绿旗时": "when @green-flag clicked",
         "结束": "end",
     },
 
     "he": { # Hebrew
-        "הסתובב שמאל _ מעלות": "turn @arrow-ccw _ degrees",
-        "הסתובב ימינה _ מעלות": "turn @arrow-cw _ degrees",
+        "הסתובב שמאל %n מעלות": "turn @arrow-ccw %n degrees",
+        "הסתובב ימינה %n מעלות": "turn @arrow-cw %n degrees",
         "כאשר לוחצים על דגל ירוק": "when @green-flag clicked",
         "סוף": "end",
     },
 
     "pl": { # Polish
-        "obróć w lewo o _ stopni": "turn @arrow-ccw _ degrees",
-        "obróć w prawo o _ stopni": "turn @arrow-cw _ degrees",
+        "obróć w lewo o %n stopni": "turn @arrow-ccw %n degrees",
+        "obróć w prawo o %n stopni": "turn @arrow-cw %n degrees",
         "kiedy kliknięto zieloną flagę": "when @green-flag clicked",
         "koniec": "end",
     },
 
     "nb": { # Norwegian
-        "vend venstre _ grader": "turn @arrow-ccw _ degrees",
-        "vend høyre _ grader": "turn @arrow-cw _ degrees",
+        "vend venstre %n grader": "turn @arrow-ccw %n degrees",
+        "vend høyre %n grader": "turn @arrow-cw %n degrees",
         "når grønt flagg klikkes": "when @green-flag clicked",
         "slutt": "end",
     },
 
     "ru": { # Russian
-        "повернуть влево на _ градусов": "turn @arrow-ccw _ degrees",
-        "повернуть вправо на _ градусов": "turn @arrow-cw _ degrees",
+        "повернуть влево на %n градусов": "turn @arrow-ccw %n degrees",
+        "повернуть вправо на %n градусов": "turn @arrow-cw %n degrees",
         "когда щёлкнут по зелёному флагу": "when @green-flag clicked",
         "конец": "end",
     },
 
     "ca": { # Catalan
-        "gira a l'esquerra _ graus": "turn @arrow-ccw _ degrees",
-        "gira a la dreta _ graus": "turn @arrow-cw _ degrees",
+        "gira a l'esquerra %n graus": "turn @arrow-ccw %n degrees",
+        "gira a la dreta %n graus": "turn @arrow-cw %n degrees",
         "quan la bandera verda es premi": "when @green-flag clicked",
         "fi": "end",
     },
 
     "tr": { # Turkish
-        "_ derece sola dön": "turn @arrow-ccw _ degrees",
-        "_ derece sağa dön": "turn @arrow-cw _ degrees",
-        "_ derece saatin tersi yönde dön": "turn @arrow-ccw _ degrees",
-        "_ derece saat yönünde dön": "turn @arrow-cw _ degrees",
+        "_ derece sola dön": "turn @arrow-ccw %n degrees",
+        "_ derece sağa dön": "turn @arrow-cw %n degrees",
+        "_ derece saatin tersi yönde dön": "turn @arrow-ccw %n degrees",
+        "_ derece saat yönünde dön": "turn @arrow-cw %n degrees",
         "yeşil bayrak tıklandığında": "when @green-flag clicked",
         "son": "end",
     },
 
     "el": { # Greek
-        "στρίψε αριστερά _ μοίρες": "turn @arrow-ccw _ degrees",
-        "στρίψε αριστερόστροφα _ μοίρες": "turn @arrow-ccw _ degrees",
-        "στρίψε δεξιά _ μοίρες": "turn @arrow-cw _ degrees",
-        "στρίψε δεξιόστροφα _ μοίρες": "turn @arrow-cw _ degrees",
+        "στρίψε αριστερά %n μοίρες": "turn @arrow-ccw %n degrees",
+        "στρίψε αριστερόστροφα %n μοίρες": "turn @arrow-ccw %n degrees",
+        "στρίψε δεξιά %n μοίρες": "turn @arrow-cw %n degrees",
+        "στρίψε δεξιόστροφα %n μοίρες": "turn @arrow-cw %n degrees",
         "Όταν στην πράσινη σημαία γίνει κλικ": "when @green-flag clicked",
         "τέλος": "end",
     },
 
     "": { # template
-        "": "turn @arrow-ccw _ degrees",
-        "": "turn @arrow-cw _ degrees",
+        "": "turn @arrow-ccw %n degrees",
+        "": "turn @arrow-cw %n degrees",
         "": "when @green-flag clicked",
         "": "end",
     },
--- src/scratchblocks.js
+++ src/scratchblocks.js
@@ -17,6 +17,7 @@ var scratchblocks = function () {
 
   function extend(src, dest) {
     src = src || {};
+    dest = dest || {};
     for (var key in src) {
       if (src.hasOwnProperty(key) && !dest.hasOwnProperty(key)) {
         dest[key] = src[key];
@@ -83,42 +84,6 @@ var scratchblocks = function () {
   // List of commands taken from Scratch
   var scratchCommands = [["move %n steps"," ",1,"forward:"],["turn @turnRight %n degrees"," ",1,"turnRight:"],["turn @turnLeft %n degrees"," ",1,"turnLeft:"],["point in direction %d.direction"," ",1,"heading:"],["point towards %m.spriteOrMouse"," ",1,"pointTowards:"],["go to x:%n y:%n"," ",1,"gotoX:y:"],["go to %m.location"," ",1,"gotoSpriteOrMouse:"],["glide %n secs to x:%n y:%n"," ",1,"glideSecs:toX:y:elapsed:from:"],["change x by %n"," ",1,"changeXposBy:"],["set x to %n"," ",1,"xpos:"],["change y by %n"," ",1,"changeYposBy:"],["set y to %n"," ",1,"ypos:"],["set rotation style %m.rotationStyle"," ",1,"setRotationStyle"],["say %s for %n secs"," ",2,"say:duration:elapsed:from:"],["say %s"," ",2,"say:"],["think %s for %n secs"," ",2,"think:duration:elapsed:from:"],["think %s"," ",2,"think:"],["show"," ",2,"show"],["hide"," ",2,"hide"],["switch costume to %m.costume"," ",2,"lookLike:"],["next costume"," ",2,"nextCostume"],["next backdrop"," ",102,"nextScene"],["switch backdrop to %m.backdrop"," ",2,"startScene"],["switch backdrop to %m.backdrop and wait"," ",102,"startSceneAndWait"],["change %m.effect effect by %n"," ",2,"changeGraphicEffect:by:"],["set %m.effect effect to %n"," ",2,"setGraphicEffect:to:"],["clear graphic effects"," ",2,"filterReset"],["change size by %n"," ",2,"changeSizeBy:"],["set size to %n%"," ",2,"setSizeTo:"],["go to front"," ",2,"comeToFront"],["go back %n layers"," ",2,"goBackByLayers:"],["play sound %m.sound"," ",3,"playSound:"],["play sound %m.sound until done"," ",3,"doPlaySoundAndWait"],["stop all sounds"," ",3,"stopAllSounds"],["play drum %d.drum for %n beats"," ",3,"playDrum"],["rest for %n beats"," ",3,"rest:elapsed:from:"],["play note %d.note for %n beats"," ",3,"noteOn:duration:elapsed:from:"],["set instrument to %d.instrument"," ",3,"instrument:"],["change volume by %n"," ",3,"changeVolumeBy:"],["set volume to %n%"," ",3,"setVolumeTo:"],["change tempo by %n"," ",3,"changeTempoBy:"],["set tempo to %n bpm"," ",3,"setTempoTo:"],["clear"," ",4,"clearPenTrails"],["stamp"," ",4,"stampCostume"],["pen down"," ",4,"putPenDown"],["pen up"," ",4,"putPenUp"],["set pen color to %c"," ",4,"penColor:"],["change pen color by %n"," ",4,"changePenHueBy:"],["set pen color to %n"," ",4,"setPenHueTo:"],["change pen shade by %n"," ",4,"changePenShadeBy:"],["set pen shade to %n"," ",4,"setPenShadeTo:"],["change pen size by %n"," ",4,"changePenSizeBy:"],["set pen size to %n"," ",4,"penSize:"],["when @greenFlag clicked","h",5,"whenGreenFlag"],["when %m.key key pressed","h",5,"whenKeyPressed"],["when this sprite clicked","h",5,"whenClicked"],["when backdrop switches to %m.backdrop","h",5,"whenSceneStarts"],["when %m.triggerSensor > %n","h",5,"whenSensorGreaterThan"],["when I receive %m.broadcast","h",5,"whenIReceive"],["broadcast %m.broadcast"," ",5,"broadcast:"],["broadcast %m.broadcast and wait"," ",5,"doBroadcastAndWait"],["wait %n secs"," ",6,"wait:elapsed:from:"],["repeat %n","c",6,"doRepeat"],["forever","cf",6,"doForever"],["if %b then","c",6,"doIf"],["if %b then","e",6,"doIfElse"],["wait until %b"," ",6,"doWaitUntil"],["repeat until %b","c",6,"doUntil"],["stop %m.stop","f",6,"stopScripts"],["when I start as a clone","h",6,"whenCloned"],["create clone of %m.spriteOnly"," ",6,"createCloneOf"],["delete this clone","f",6,"deleteClone"],["ask %s and wait"," ",7,"doAsk"],["turn video %m.videoState"," ",7,"setVideoState"],["set video transparency to %n%"," ",7,"setVideoTransparency"],["reset timer"," ",7,"timerReset"],["set %m.var to %s"," ",9,"setVar:to:"],["change %m.var by %n"," ",9,"changeVar:by:"],["show variable %m.var"," ",9,"showVariable:"],["hide variable %m.var"," ",9,"hideVariable:"],["add %s to %m.list"," ",12,"append:toList:"],["delete %d.listDeleteItem of %m.list"," ",12,"deleteLine:ofList:"],["if on edge,bounce"," ",1,"bounceOffEdge"],["insert %s at %d.listItem of %m.list"," ",12,"insert:at:ofList:"],["replace item %d.listItem of %m.list with %s"," ",12,"setLine:ofList:to:"],["show list %m.list"," ",12,"showList:"],["hide list %m.list"," ",12,"hideList:"],["x position","r",1,"xpos"],["y position","r",1,"ypos"],["direction","r",1,"heading"],["costume #","r",2,"costumeIndex"],["size","r",2,"scale"],["backdrop name","r",102,"sceneName"],["backdrop #","r",102,"backgroundIndex"],["volume","r",3,"volume"],["tempo","r",3,"tempo"],["touching %m.touching?","b",7,"touching:"],["touching color %c?","b",7,"touchingColor:"],["color %c is touching %c?","b",7,"color:sees:"],["distance to %m.spriteOrMouse","r",7,"distanceTo:"],["answer","r",7,"answer"],["key %m.key pressed?","b",7,"keyPressed:"],["mouse down?","b",7,"mousePressed"],["mouse x","r",7,"mouseX"],["mouse y","r",7,"mouseY"],["loudness","r",7,"soundLevel"],["video %m.videoMotionType on %m.stageOrThis","r",7,"senseVideoMotion"],["timer","r",7,"timer"],["%m.attribute of %m.spriteOrStage","r",7,"getAttribute:of:"],["current %m.timeAndDate","r",7,"timeAndDate"],["days since 2000","r",7,"timestamp"],["username","r",7,"getUserName"],["%n + %n","r",8,"+"],["%n - %n","r",8,"-"],["%n * %n","r",8,"*"],["%n / %n","r",8,"/"],["pick random %n to %n","r",8,"randomFrom:to:"],["%s < %s","b",8,"<"],["%s = %s","b",8,"="],["%s > %s","b",8,">"],["%b and %b","b",8,"&"],["%b or %b","b",8,"|"],["not %b","b",8,"not"],["join %s %s","r",8,"concatenate:with:"],["letter %n of %s","r",8,"letter:of:"],["length of %s","r",8,"stringLength:"],["%n mod %n","r",8,"%"],["round %n","r",8,"rounded"],["%m.mathOp of %n","r",8,"computeFunction:of:"],["item %d.listItem of %m.list","r",12,"getLine:ofList:"],["length of %m.list","r",12,"lineCountOfList:"],["%m.list contains %s?","b",12,"list:contains:"],["%m.var","r",9,"readVariable"],["%m.list","r",12,"contentsOfList:"],["%m.param","r",11,"getParam"],["%m.param","b",11,"getParam"],["else","else",6,"else"],["end","end",6,"end"],["...","ellips",42,"ellips"],]; 
 
-  var english = {
-    code: "en",
-
-    aliases: {
-      "turn left _ degrees": "turn @turnLeft _ degrees",
-      "turn ccw _ degrees": "turn @turnLeft _ degrees",
-      "turn ↺ _ degrees": "turn @turnLeft _ degrees",
-      "turn right _ degrees": "turn @turnRight _ degrees",
-      "turn cw _ degrees": "turn @turnRight _ degrees",
-      "turn ↻ _ degrees": "turn @turnRight _ degrees",
-      "when gf clicked": "when @greenFlag clicked",
-      "when flag clicked": "when @greenFlag clicked",
-      "when green flag clicked": "when @greenFlag clicked",
-      "when ⚑ clicked": "when @greenFlag clicked",
-    },
-
-    define: ["define"],
-
-    // For ignoring the lt sign in the "when distance < _" block
-    ignorelt: ["when distance"],
-
-    // Valid arguments to "of" dropdown, for resolving ambiguous situations
-    math: ["abs", "floor", "ceiling", "sqrt", "sin", "cos", "tan", "asin", "acos", "atan", "ln", "log", "e ^", "10 ^"],
-
-    // For detecting the "stop" cap / stack block
-    osis: ["other scripts in sprite", "other scripts in stage"],
-
-    commands: scratchCommands,
-  };
-
-  var image_text = {
-    "turnRight": "↻",
-    "turnLeft": "↺",
-  };
-
-
   var categoriesById = {
     1:  "motion",
     2:  "looks",
@@ -157,137 +122,113 @@ var scratchblocks = function () {
     return {
       spec: spec,
       parts: parts,
-      parts.filter(function(p) { return inputPat.test(p); }),
+      inputs: parts.filter(function(p) { return inputPat.test(p); }),
+      hash: hashSpec(spec),
     };
   }
 
+  function hashSpec(spec) {
+    return spec.replace(inputPat, " _ ").replace(/ +/g, " ").trim();
+  }
+
   var blocksBySelector = {};
-  var blocks = scratchCommands.map(function(command) {
+  var blocksBySpec = {};
+  var allBlocks = scratchCommands.map(function(command) {
     assert(command.length <= 4); // TODO
-    var block = extend(parseSpec(command[0], {
+    var info = extend(parseSpec(command[0]), {
       shape: typeShapes[command[1]], // /[ bcefhr]|cf/
       category: categoriesById[command[2] % 100],
       selector: command[3],
     });
-    if (block.selector !== 'getParam') assert(!blocksBySelector[block.selector], block.selector);
-    return blocksBySelector[block.selector] = block;
+    if (info.selector !== 'getParam') assert(!blocksBySelector[info.selector], info.selector);
+    return blocksBySelector[info.selector] = blocksBySpec[info.spec] = info;
   });
 
+  var imageIcons = {
+    "@greenFlag": "⚑",
+    "@turnRight": "↻",
+    "@turnLeft": "↺",
+  };
 
+  var allLanguages = {};
+  function loadLanguage(code, language) {
+    var blocksByHash = language.blocksByHash = {};
 
-
-
-  // The blockids are the same as english block text, so we build the blockid
-  // list at the same time.
-
-  var category = null;
-  for (var i=0; i<englishCommands.length; i++) {
-    if (englishCommands[i].length === 1) { // [category]
-      category = english_blocks[i][0];
-    } else {                              // [block id, [list of flags]]
-      var block_and_flags = english_blocks[i],
-          spec = block_and_flags[0], flags = block_and_flags[1];
-      english.blocks.push(spec);
-
-      blockids.push(spec); // Other languages will just provide a list of
-      // translations, which is matched up with this
-      // list.
-
-      // Now store shape/category info.
-      var info = {
-        blockid: spec,
-        category: category,
+    Object.keys(language.commands).forEach(function(spec) {
+      var nativeSpec = language.commands[spec];
+      var block = {
+        language: code,
+        block: blocksBySpec[spec],
       };
 
-      while (flags.length) {
-        var flag = flags.pop();
-        switch (flag) {
-          case "hat":
-          case "cap":
-            info.shape = flag;
-            break;
-          default:
-            assert(!info.flag);
-            info.flag = flag;
-        }
+      var nativeHash = hashSpec(nativeSpec);
+      blocksByHash[nativeHash] = block;
+
+      // fallback image replacement
+      // not every language has `aliases` yet
+      var m = /@[a-zA-Z]+/.exec(spec);
+      if (m) {
+        var image = m[0];
+        var hash = nativeHash.replace(image, imageIcons[image]);
+        blocksByHash[nativeHash] = block;
       }
+    });
 
-      var image_match = /@([-A-z]+)/.exec(spec);
-      if (image_match) {
-        info.image_replacement = image_match[1];
-      }
-
-      block_info_by_id[spec] = info;
-    }
-  }
-
-  // Built english, now add it.
-
-  load_language(english);
-
-  function load_language(language) {
-    language = clone(language);
-
-    var iso_code = language.code;
-    delete language.code;
-
-    // convert blocks list to a dict.
-    var block_spec_by_id = {};
-    for (var i=0; i<language.blocks.length; i++) {
-      var spec = language.blocks[i],
-          blockid = blockids[i];
-      spec = spec.replace(/@[-A-z]+/, "@"); // remove images
-      block_spec_by_id[blockid] = spec;
-
-      // Add block to the text lookup dict.
-      var minispec = minify(normalize_spec(spec));
-      if (minispec) block_by_text[minispec] = {
-        blockid: blockid,
-        lang: iso_code,
+    Object.keys(language.aliases).forEach(function(alias) {
+      var spec = language.aliases[alias];
+      var block = {
+        language: code,
+        block: blocksBySpec[spec],
       };
-    }
-    language.blocks = block_spec_by_id;
 
-    // add aliases (for images)
-    for (var text in language.aliases) {
-      strings.aliases[text] = language.aliases[text];
+      var aliasHash = hashSpec(alias);
+      blocksByHash[aliasHash] = block;
+    });
 
-      // Add alias to the text lookup dict.
-      var minispec = minify(normalize_spec(text));
-      block_by_text[minispec] = {
-        blockid: language.aliases[text],
-        lang: iso_code,
-      };
-    }
-
-    // add stuff to strings
-    for (var key in strings) {
-      if (strings[key].constructor === Array) {
-        for (i=0; i<language[key].length; i++) {
-          if (language[key][i]) {
-            strings[key].push(minify(language[key][i]));
-          }
-        }
-      }
-    }
-
-    languages[iso_code] = language;
+    allLanguages[code] = language;
   }
-  load_language = load_language;
-
-  // Store initial state.
-  var _init_strings = clone(strings);
-  var _init_languages = clone(languages);
-  var _init_block_by_text = clone(block_by_text);
-
-  var reset_languages = function(language) {
-    strings = clone(_init_strings);
-    languages = clone(_init_languages);
-    block_by_text = clone(_init_block_by_text);
+  function loadLanguages(languages) {
+    Object.keys(languages).forEach(function(code) {
+      loadLanguage(code, languages[code]);
+    });
   }
+
+  var english = {
+    aliases: {
+      "turn left %n degrees": "turn @turnLeft %n degrees",
+      "turn ccw %n degrees": "turn @turnLeft %n degrees",
+      "turn right %n degrees": "turn @turnRight %n degrees",
+      "turn cw %n degrees": "turn @turnRight %n degrees",
+      "when gf clicked": "when @greenFlag clicked",
+      "when flag clicked": "when @greenFlag clicked",
+      "when green flag clicked": "when @greenFlag clicked",
+    },
+
+    define: ["define"],
+
+    // For ignoring the lt sign in the "when distance < _" block
+    ignorelt: ["when distance"],
+
+    // Valid arguments to "of" dropdown, for resolving ambiguous situations
+    math: ["abs", "floor", "ceiling", "sqrt", "sin", "cos", "tan", "asin", "acos", "atan", "ln", "log", "e ^", "10 ^"],
+
+    // For detecting the "stop" cap / stack block
+    osis: ["other scripts in sprite", "other scripts in stage"],
+
+    commands: {},
+  };
+  allBlocks.forEach(function(info) {
+    english.commands[info.spec] = info.spec;
+  }),
+  loadLanguages({
+    en: english,
+  });
+
+  /*****************************************************************************/
 
   // Hacks for certain blocks. TODO
 
+  /*
   block_info_by_id["_ of _"].hack = function (info, args) {
     // Operators if math function, otherwise sensing "attribute of" block
     if (!args.length) return;
@@ -352,15 +293,85 @@ var scratchblocks = function () {
   function normalize_spec(spec) {
     return spec.replace(/([^ ])_/g, "$1 _").replace(/_([^ ])/g, "_ $1");
   }
+  */
 
+  // TODO paint blocks
   // TODO recognise list reporters
   // TODO custom arguments
   // TODO definitions
   // TODO ignoreLt
   // TODO comparisons vs. predicates
 
-  function parse(code) {
-    return [];
+  function parse(code, options) {
+    var options = extend({
+      inline: false,
+      languages: ['en'],
+    }, options);
+
+    var languages = options.languages.map(function(code) {
+      return allLanguages[code];
+    });
+
+    var tok = code[0];
+    var index = 0;
+    function next() {
+      tok = code[++index];
+    }
+    function consume(what) {
+      if (tok === what) {
+        next();
+        return true;
+      }
+      return false;
+    }
+
+    /* * */
+
+    function pFile() {
+      while (consume("\n")) {}
+      var scripts = [];
+      while (tok) {
+        var blocks = [];
+        while (tok && tok !== '\n') {
+          var b = pBlock();
+          if (b.isHat) {
+            if (blocks.length) scripts.push(new Script(blocks));
+            blocks = [b];
+          } else if (b.isFinal) {
+            blocks.push(b);
+            break;
+          } else if (b.isCommand) {
+            blocks.push(b);
+          } else { // reporter or predicate
+            if (blocks.length) scripts.push(new Script(blocks));
+            scripts.push(new Script([b]));
+            blocks = [];
+            break;
+          }
+        }
+        if (blocks.length) scripts.push(new Script(blocks));
+        while (consume("\n")) {}
+      }
+      return scripts;
+    }
+
+    function pBlock() {
+      var x = tok;
+      next();
+      consume('\n');
+      var shape = {
+        's': 'stack',
+        'c': 'cap',
+        'h': 'hat',
+        'r': 'reporter',
+        'b': 'boolean',
+      }[x];
+      return new Block({
+        shape: shape,
+      }, [new Label(shape)]);
+    }
+
+    return pFile();
   }
 
   /*****************************************************************************/
@@ -808,17 +819,20 @@ var scratchblocks = function () {
       class: cls || '',
     });
     this.width = null;
-    if (value === "") {
-      this.width = 0;
-    } else if (value === " ") {
-      this.width = 4.15625;
-    } else {
-      Label.measure(this);
-    }
     this.height = 12;
     this.x = 0;
   };
   Label.prototype.isLabel = true;
+
+  Label.prototype.measure = function() {
+    if (this.value === "") {
+      this.width = 0;
+    } else if (this.value === " ") {
+      this.width = 4.15625;
+    } else {
+      Label.measure(this);
+    }
+  };
 
   Label.prototype.draw = function() {
     return this.el;
@@ -983,6 +997,13 @@ var scratchblocks = function () {
     this.x = 0;
   };
   Block.prototype.isBlock = true;
+
+  Block.prototype.measure = function() {
+    for (var i=0; i<this.children.length; i++) {
+      var child = this.children[i];
+      if (child) child.measure();
+    }
+  };
 
   Block.shapes = {
     'stack': stackRect,
@@ -1163,6 +1184,12 @@ var scratchblocks = function () {
   };
   Script.prototype.isScript = true;
 
+  Script.prototype.measure = function() {
+    for (var i=0; i<this.blocks.length; i++) {
+      this.blocks[i].measure();
+    }
+  };
+
   Script.prototype.draw = function() {
     var children = [];
     var y = 0;
@@ -1183,9 +1210,12 @@ var scratchblocks = function () {
 
   /*****************************************************************************/
 
-
   function render(scripts, cb) {
     Label.startMeasuring();
+
+    scripts.forEach(function(script) {
+      script.measure();
+    });
 
     // measure strings, then draw
     Label.endMeasuring(drawScripts.bind(null, scripts, cb));
@@ -1233,10 +1263,10 @@ var scratchblocks = function () {
    * Like the old 'scratchblocks2.parse().
    */
   var renderMatching = function (selector, options) {
-    selector = selector || "pre.blocks";
-    options = options || {
+    var selector = selector || "pre.blocks";
+    var options = extend({
       inline: false,
-    }
+    }, options);
 
     // find elements
     var results = [].slice.apply(document.querySelectorAll(selector));
@@ -1250,7 +1280,7 @@ var scratchblocks = function () {
         code = code.replace('\n', '');
       }
 
-      var scripts = parse(code);
+      var scripts = parse(code, options);
       render(scripts, function(svg) {
         var container = document.createElement('div');
         container.classList.add("sb");
@@ -1266,14 +1296,13 @@ var scratchblocks = function () {
 
 
   return {
+    allLanguages: allLanguages, // read-only
+    loadLanguages: loadLanguages,
+
     parse: parse,
     render: render,
     renderMatching: renderMatching,
     exportSVG: exportSVG,
-
-    resetLanguages: reset_languages,
-    loadLanguage: load_language,
-    _findBlock: find_block,
   };
 
 }();
