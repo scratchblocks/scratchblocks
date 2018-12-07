7fa18cc5b2129ab089c8aae2c41ad93290ef7471
Jonic Linley <jonic@100yen.co.uk>
2018-12-07 11:42:04 +0000

    Progress

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,23 @@
 import babel from "rollup-plugin-babel"
+import builtins from "rollup-plugin-node-builtins"
 import commonjs from "rollup-plugin-commonjs"
+import conditional from "rollup-plugin-conditional"
+import globals from "rollup-plugin-node-globals"
 import minify from "rollup-plugin-babel-minify"
 import pkg from "./package.json"
 import resolve from "rollup-plugin-node-resolve"
 import serve from "rollup-plugin-serve"
+
+let { buildTarget } = process.env
+
+if (typeof buildTarget === "undefined") {
+  buildTarget = "DEV"
+}
+
+const env = {
+  dev: buildTarget === "DEV",
+  prod: buildTarget === "PROD",
+}
 
 export default [
   {
@@ -11,29 +25,32 @@ export default [
     output: [
       {
         file: pkg.main,
-        format: "cjs",
+        format: "iife",
+        name: "scratchblocks",
         sourcemap: true,
       },
       {
         file: pkg.module,
-        format: "es",
+        format: "esm",
         sourcemap: true,
       },
     ],
     plugins: [
       resolve(),
       commonjs(),
-      babel({
-        exclude: "node_modules/**",
-      }),
-      minify({
-        comments: false,
-      }),
-      serve({
-        contentBase: ".",
-        open: true,
-        port: 8000,
-      }),
+      babel(),
+      globals(),
+      builtins(),
+      // // minify({
+      // //   comments: false,
+      // // }),
+      conditional(env.dev, () => [
+        serve({
+          contentBase: ".",
+          // open: true,
+          port: 8000,
+        }),
+      ]),
     ],
   },
 ]
