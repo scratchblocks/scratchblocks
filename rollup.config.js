b1a5c38ea42a127d7ec43454f057b84e1f0c87ea
Jonic Linley <jonic@100yen.co.uk>
2018-12-07 22:22:55 +0000

    Removed conditional plugin

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,6 @@
 import babel from "rollup-plugin-babel"
 import builtins from "rollup-plugin-node-builtins"
 import commonjs from "rollup-plugin-commonjs"
-import conditional from "rollup-plugin-conditional"
 import globals from "rollup-plugin-node-globals"
 import minify from "rollup-plugin-babel-minify"
 import pkg from "./package.json"
