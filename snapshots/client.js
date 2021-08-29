ecf724666321e11f25fdd900e2ccfc46a2613849
Tim Radvan <tim@tjvr.org>
2021-08-29 10:06:01 +0100

    Re-run prettier

--- snapshots/client.js
+++ snapshots/client.js
@@ -11,7 +11,7 @@ window.render = function(source, options, scale) {
 
   var view = scratchblocks.newView(doc, {
     style: options.style,
-    scale: options.scale
+    scale: options.scale,
   })
   var svg = view.render()
 
