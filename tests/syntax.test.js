ba036dc6c207d97c7199406e45e02e9774a54a08
apple502j <33279053+apple502j@users.noreply.github.com>
2023-01-26 21:30:58 +0900

    Fix tests

--- tests/syntax.test.js
+++ tests/syntax.test.js
@@ -1,11 +1,11 @@
 import { parse, loadLanguages, allLanguages } from "../syntax"
 
-import de from "../locales/de.json"
-import ja from "../locales/ja.json"
-import ko from "../locales/ko.json"
-import pt_br from "../locales/pt-br.json"
-import rap from "../locales/rap.json"
-import uz from "../locales/uz.json"
+import de from "../locales/de.json" assert { "type": "json" }
+import ja from "../locales/ja.json" assert { "type": "json" }
+import ko from "../locales/ko.json" assert { "type": "json" }
+import pt_br from "../locales/pt-br.json" assert { "type": "json" }
+import rap from "../locales/rap.json" assert { "type": "json" }
+import uz from "../locales/uz.json" assert { "type": "json" }
 
 loadLanguages({
   de,
