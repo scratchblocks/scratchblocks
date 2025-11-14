89e19fe3e78a87a8ee00d6f16cc59c51f4d024c5
Lu Yifei <118034848+LuYifei2011@users.noreply.github.com>
2025-11-14 18:44:57 +0800

    Optimize overrides logic to avoid unnecessary repeated assignments

--- syntax/blocks.js
+++ syntax/blocks.js
@@ -458,10 +458,10 @@ export function applyOverrides(info, overrides) {
       info.color = name
       info.category = ""
       info.categoryIsDefault = false
-    } else if (overrideCategories.includes(name)) {
+    } else if (overrideCategories.includes(name) && info.category !== name) {
       info.category = name
       info.categoryIsDefault = false
-    } else if (overrideShapes.includes(name)) {
+    } else if (overrideShapes.includes(name) && info.shape !== name) {
       info.shape = name
       info.shapeIsDefault = false
     } else if (name === "loop") {
