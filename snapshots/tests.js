da2b6d0d12c298da9c7e39972fa603a964d5429a
apple502j <33279053+apple502j@users.noreply.github.com>
2026-06-24 11:29:43 +0900

    Add German Face Sensing blocks to snapshots

--- snapshots/tests.js
+++ snapshots/tests.js
@@ -1306,6 +1306,17 @@ Wenn Kraftsensor [gedrückt v]
 <fallend?>
 (Rotationsgeschwindigkeit [z v])
 (Beschleunigung [x v])
+
+// Face Sensing
+gehe zu [Nase v]
+Richtung der Gesichtsneigung zeigen
+setze Größe auf Gesichtsgröße
+wenn Gesicht kippt [nach links v]
+wenn diese Figur ein [Nase v] berührt
+wenn ein Gesicht erkannt wird
+<wird ein Gesicht erkannt?>
+(Kopfneigung)
+(Gesichtsgröße)
 `,
   "de",
 )
